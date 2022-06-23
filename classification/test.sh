d89553d08cead13b8843513dbbb9285021b1f577
weixuansun <weixuansun7@gmail.com>
2022-06-24 00:09:55 +0800

    Update test.sh

--- classification/test.sh
+++ classification/test.sh
@@ -1,21 +1,22 @@
-#!/usr/bin/env bash
 export NCCL_LL_THRESHOLD=0
 
 CONFIG=$1
 GPUS=$2
-PORT=$3
-N_TASK_PER_NODE=1
+RESUME=$3
+PORT=${PORT:-99998}
+N_TASK_PER_NODE=$(($2<8?$2:8))
 batch_size=20
 OUTPUT_DIR=checkpoints/test/
-DATA_PATH=/mnt/lustre/share_data/qinzhen/images/images/
+DATA_PATH=/your/data/path/
 
-# slurm
-# spring.submit arun --gpu -n$GPUS --cpus-per-task 1 --ntasks-per-node $N_TASK_PER_NODE -p M3T --quotatype=auto \
-# "
-# python main.py --config $CONFIG  --data-set CIFAR --batch-size $batch_size --dist-eval --data-path $DATA_PATH \
-# --output_dir $OUTPUT_DIR --port $PORT \
-# "
+
+spring.submit arun --gpu -n$GPUS --cpus-per-task 4 --ntasks-per-node $N_TASK_PER_NODE -p MMG --quotatype=auto \
+"
+python main.py --config $CONFIG  --data-set IMNET --batch-size $batch_size --dist-eval --data-path $DATA_PATH \
+--output_dir $OUTPUT_DIR --port $PORT --resume $RESUME  --eval \
+"
 
 # local multi/single GPU
-# CUDA_VISIBLE_DEVICES=0 python -m torch.distributed.launch --nproc_per_node=$GPUS --master_port=$PORT \
-#     --use_env main.py --config $CONFIG  --data-set CIFAR --batch-size $batch_size --output_dir $OUTPUT_DIR \
+# python -m torch.distributed.launch --nproc_per_node=$GPUS --master_port=$PORT \
+#     --use_env main.py --config $CONFIG  --data-set IMNET --batch-size $batch_size --output_dir $OUTPUT_DIR --resume $RESUME --eval\
+
