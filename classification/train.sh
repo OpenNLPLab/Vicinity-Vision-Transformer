17972386bbac598fef78f97d7a26aa4201530611
Doraemonzzz <715073608@qq.com>
2022-12-19 13:18:53 +0800

    update train code

--- classification/train.sh
+++ classification/train.sh
@@ -1,22 +1,28 @@
+#!/usr/bin/env bash
 export NCCL_LL_THRESHOLD=0
+# export NCCL_IB_DISABLE=1
 
-CONFIG=$1
-GPUS=$2
-PORT=${PORT:-99998}
-N_TASK_PER_NODE=$(($2<8?$2:8))
-batch_size=20
-OUTPUT_DIR=checkpoints/test/
-DATA_PATH=/your/data/path/
-
-
-spring.submit arun --gpu -n$GPUS --cpus-per-task 4 --ntasks-per-node $N_TASK_PER_NODE -p MMG --quotatype=auto \
-"
-python main.py --config $CONFIG  --data-set IMNET --batch-size $batch_size --dist-eval --data-path $DATA_PATH \
---output_dir $OUTPUT_DIR --port $PORT \
-"
-
-# local multi/single GPU
-# python -m torch.distributed.launch --nproc_per_node=$GPUS --master_port=$PORT \
-#     --use_env main.py --config $CONFIG  --data-set IMNET --batch-size $batch_size --output_dir $OUTPUT_DIR \
+CONFIG=$2
+GPUS=$1
+NODES=2
+batch_size=$3
+PORT=${PORT:-8889}
+OUTPUT_DIR=$4
+RESUME=$5
 
 
+if [ -z $RESUME ]
+then
+    spring.submit arun --gpu -n$GPUS --cpus-per-task 4   --ntasks-per-node 8 -p MMG  \
+    "
+    python main.py --config $CONFIG  --data-set IMNET --batch-size $batch_size --dist-eval \
+    --output_dir $OUTPUT_DIR \
+    "
+else
+    echo $RESUME
+    spring.submit arun --gpu -n$GPUS --cpus-per-task 4   --ntasks-per-node 8 -p MMG \
+    "
+    python main.py --config $CONFIG  --data-set IMNET --batch-size $batch_size --dist-eval \
+    --output_dir $OUTPUT_DIR --resume $RESUME \
+    "
+fi
